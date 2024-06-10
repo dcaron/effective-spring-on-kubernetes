be3b655e15f5f5a0235777f6ae73aff36e329436
dcaron <dcaron@xs4all.nl>
2024-06-10 23:59:39 +0200

    Updated to Spring Boot  3.3.0

--- containerize/custom-layers-dockerfile/src/main/java/com/example/SpringSecurityConfiguration.java
+++ containerize/custom-layers-dockerfile/src/main/java/com/example/SpringSecurityConfiguration.java
@@ -1,14 +1,22 @@
 package com.example;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
 @EnableWebSecurity
-class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+class SpringSecurityConfiguration {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().mvcMatchers("/**").permitAll();
-  }
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().permitAll());
+        return http.build();
+    }
 }
